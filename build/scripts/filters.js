69ba562537bee7c5995c46cc312b2584c2c04f78
Jonathan Porta <jonathan@jonathanporta.com>
2012-12-24 11:22:16 -0700

    Removed build folder from repo

--- build/scripts/filters.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  var Short;
-  Short = function(){
-    return function(id){
-      return "(" + id.substring(0, 8) + ")";
-    };
-  };
-  angular.module('AppModule').filter('shortId', Short);
-}).call(this);
